e75ecf0ae8644a13a22a5738048784cd0c2bae3c
pabombs <pabombs@gmail.com>
2015-03-06 10:14:38 -0500

    I added the interface so that you guys can put your constants
    use all uppercase names for  final variables and please try to stick to formatting

---
+++ src/caseStudy/IConstants.java
@@ -0,0 +1,14 @@
+package caseStudy;
+
+public interface IConstants {
+    
+    //ADD YOUR STUFF HERE, try to keep dome fomatting
+    
+    //Pendulum
+    public static final double GRAVACC = 9.80665;
+    //
+    
+    //SPRING
+    //------
+    //
+}
