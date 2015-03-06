01b4f08b292b6cc2300c1e9ab41373691b456802
pabombs <pabombs@gmail.com>
2015-03-06 10:22:04 -0500

    added a start method to case study
    CaseStudy class is the main one, evverything is gonna start from there boyz

---
+++ src/caseStudy/CaseStudy.java
@@ -0,0 +1,12 @@
+package caseStudy;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CaseStudy extends Application{
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.print("potat");
+    }
+}
