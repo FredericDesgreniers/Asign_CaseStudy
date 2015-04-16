58dae6d336da0d42917743c21a71890c327f8b99
pabombs <pabombs@gmail.com>
2015-04-16 13:53:22 -0400

    adde for loop and made stuff work

--- src/caseStudy/animation/Spring.java
+++ src/caseStudy/animation/Spring.java
@@ -6,12 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
 public final class Spring extends AnimationBase implements IConstants{
@@ -45,7 +42,7 @@ public final class Spring extends AnimationBase implements IConstants{
         spring.setFitHeight(100);
         spring.setPreserveRatio(true);
         spring.setSmooth(true);
-        spring.setLayoutX(0);
+        spring.setLayoutX(400);
         getChildren().add(spring);
         
         Image massGraphic = new Image(this.getClass().getResourceAsStream("/res/MassCaseStudy.png"));
@@ -53,50 +50,33 @@ public final class Spring extends AnimationBase implements IConstants{
         hangingMass.setFitHeight(100);
         hangingMass.setPreserveRatio(true);
         hangingMass.setSmooth(true);
-        hangingMass.setLayoutX(0);
+        hangingMass.setLayoutX(400);
+        hangingMass.setLayoutY(spring.getFitHeight()-30);
         hangingMass.setScaleX(0.7);
         hangingMass.setScaleY(0.7);
         getChildren().add(hangingMass);
     }
     
+    //calculates the period so that the loop only calculates what is necessary
     public double calculatePeriod(){
         return 2*IConstants.PI*Math.sqrt(getMass()/getSpringConstant());
     }
     
+    //Adds the correct KeyFrames to the timeline for both animated objects
     public void calculateKeyframes(){
-        int i = 0;
         KeyFrame[] frames = new KeyFrame[(int)(calculatePeriod()*1/2*1000)];
-        while(i < frames.length){
+        for(int i = 0 ; i < frames.length ; i++){
+            //creates an easy to use value to animate with
             double stretchPercent = 1+Math.cos(((float)i*33.0f/1000.0f)*Math.sqrt(springConstant/mass));
-            //spring.setFitHeight((stretchPercent*0.6+20)*spring.getFitHeight());
-            KeyValue stretchVal = new KeyValue(spring.scaleYProperty(), (stretchPercent*0.6 + 0.4));
-            KeyValue positionVal = new KeyValue(spring.yProperty(), -(spring.getFitHeight() - (stretchPercent*0.6 + 0.4)*spring.getFitHeight())/2);
-            frames[i] = new KeyFrame(Duration.millis(33*i), stretchVal, positionVal);
-            //hangingMass.setCenterY(30.00 + (DIM_Y/2)*stretchPercent);
+            //Creates KeyValues for the spring's stretch and position
+            KeyValue stretchSpringVal = new KeyValue(spring.scaleYProperty(), (stretchPercent*0.6 + 0.4));
+            KeyValue positionSpringVal = new KeyValue(spring.yProperty(), -(spring.getFitHeight() - (stretchPercent*0.6 + 0.4)*spring.getFitHeight())/2);
+            //Creates KeyValues for the hangingMass's position
+            KeyValue positionMassVal = new KeyValue(hangingMass.yProperty(), ((stretchPercent*0.6 + 0.4)*spring.getFitHeight()-spring.getFitHeight()));
+            //Creates KeyFrames and adds them to the array
+            frames[i] = new KeyFrame(Duration.millis(33*i), stretchSpringVal, positionSpringVal, positionMassVal);
             timeline.getKeyFrames().add(frames[i]);
-            i++;
         }
-        timeline.getKeyFrames().addAll(frames);
-    }
-    
-    public void calculateKeyframes2(){
-        double stretchPercent = 0.00;
-        //adds the keyframes to the timeline
-        int tempI=-1;
-        List<KeyFrame> frames=new ArrayList();
-        for(int i = 1 ; i<100 ; i++){
-            tempI=i;
-            //calculate percent extension
-            //stretchPercent = 1+Math.cos((i*33/1000)*Math.sqrt(springConstant/mass));
-            //spring
-            //spring.setFitHeight((stretchPercent*0.6+20)*spring.getFitHeight()); //this should be adding a keyframe
-            //mass
-            //hangingMass.setCenterY(30.00 + (DIM_Y/2)*stretchPercent); //CHANGE FINAL POSITIONS
-            KeyFrame kf=new KeyFrame(Duration.millis(100+i*100),new KeyValue(spring.xProperty(),100+i*5 ));
-            System.out.println(i);
-            frames.add(kf);
-        }
-       
         timeline.getKeyFrames().addAll(frames);
     }
     
