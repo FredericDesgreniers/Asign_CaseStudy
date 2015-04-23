9dc3b69d0cf10095ff053ea1657319e2ca0df6b6
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2015-04-23 17:55:42 -0400

    Constants for SERIES

--- src/caseStudy/IConstants.java
+++ src/caseStudy/IConstants.java
@@ -1,5 +1,8 @@
 package caseStudy;
 
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+
 public interface IConstants {
     //FRAME
     public static final double DIM_X=600;
@@ -19,21 +22,21 @@ public interface IConstants {
     public static final String AN_WAVES2="PENDULUM";
     
     //ADD YOUR STUFF HERE, try to keep dome fomatting
-    
+    public static final int ZERO=0;
+    public static final int TIME_MILLIS_CONVERSION=1000;
     //Pendulum
     public static final double GRAVACC = 9.80665;
     //
     
     //SPRING
     public static final double PI = 3.14159265359;
-    
-    
-    
+    public static final Font FONT_ARIAL_20=new Font("Arial",20);
+
     //series
     public static final int SERIES_MAXITERATIONS=100;
     public static final int SERIES_OPACITY_IN=0;
     public static final int SERIES_OPACITY_FN=1;
-    
+    public static final int SERIES_TIME_SUM_EX=500;
     public static final double SERIES_A_DEF=1;
     public static final double SERIES_R_DEF=2;
     public static final double SERIES_INFINITY_IN=-1;
@@ -46,8 +49,24 @@ public interface IConstants {
     public static final double SERIES_VALUE_Y=100;
     public static final double SERIES_VALUET_Y=90;
     public static final double SERIES_VALUET_TY=-10;
+    public static final double SERIES_VALUERT_Y=30;
+    public static final double SERIES_VALUERT_X=50;
+    public static final double SERIES_VALUEAT_X=30;
+    public static final double SERIES_VALUECIRCLE_XI=200;
+    public static final double SERIES_VALUECIRCLE_XF=500;
+    public static final double SERIES_MAX_SUM_R=500;
+    public static final double SERIES_INFINITE_RANGE[]={-1,1};
     
     public static final String SERIES_VALUET_COLOR="white";
     public static final String SERIES_LABEL_STYLE="circleLabel";
-   
+    public static final String SERIES_LABEL_FORMULA_T="Geometric series: AR^n";
+    public static final String SERIES_LABEL_A_T="  set A ";
+    public static final String SERIES_LABEL_R_T="  set R ";
+    
+    
+   //style
+    public static final Paint PAINT_BLACK=Paint.valueOf("black");
+    public static final Paint PAINT_RED=Paint.valueOf("red");
+    
+    
 }
--- src/caseStudy/animation/Series.java
+++ src/caseStudy/animation/Series.java
@@ -24,7 +24,7 @@ import javafx.util.Duration;
  * COMMENTARY ADDED SINCE ALL IS MESSY AND HARD TO UNDERSTAND
  * @author Fred
  */
-public class Series extends AnimationBase{
+public class Series extends AnimationBase implements IConstants{
 
     TextField valueAText;
     TextField valueRText;
@@ -34,32 +34,37 @@ public class Series extends AnimationBase{
     public Series(String name) {
         super(name);
         //Creates all the layout stuff, nothing special
-        valueAText=new TextField(String.valueOf(IConstants.SERIES_A_DEF));
-        valueRText=new TextField(String.valueOf(IConstants.SERIES_R_DEF));
-        circleSum=new Circle();
-        radius=IConstants.SERIES_SUM_RADIUS_IN;
-        circleSum.setRadius(radius);
-        circleSum.setLayoutX(IConstants.SERIES_SUM_X);
-        circleSum.setLayoutY(IConstants.SERIES_SUM_Y);
-        infinity=IConstants.SERIES_INFINITY_IN;
-        valueRText.setLayoutY(30);
-        valueRText.setLayoutX(50);
-        valueAText.setLayoutX(50);
+
         
+        setupGui();
+    }
+    public void setupGui()
+    {
+       valueAText=new TextField(String.valueOf(SERIES_A_DEF));
+        valueRText=new TextField(String.valueOf(SERIES_R_DEF));
+        circleSum=new Circle();
+        radius=SERIES_SUM_RADIUS_IN;
+        circleSum.setRadius(radius);
+        circleSum.setLayoutX(SERIES_SUM_X);
+        circleSum.setLayoutY(SERIES_SUM_Y);
+        infinity=SERIES_INFINITY_IN;
+        valueRText.setLayoutY(SERIES_VALUERT_Y);
+        valueRText.setLayoutX(SERIES_VALUERT_X);
+        valueAText.setLayoutX(SERIES_VALUEAT_X);
         setStaticGUI();
     }
     //When you forget to add sutff to the plan so you cant add 20 variables so you just decide to add a function and make it messy
     public void setStaticGUI()
     {
-        Label formula=new Label("Geometric series: AR^n");
+        Label formula=new Label(SERIES_LABEL_FORMULA_T);
         formula.setLayoutX(30);
         formula.setLayoutY(55);
-        formula.setFont(new Font("Arial",20));
-        Label aLabel=new Label("  set A ");
-        aLabel.setTextFill(Paint.valueOf("black"));
-        Label rLabel=new Label("  set R ");
+        formula.setFont(FONT_ARIAL_20);
+        Label aLabel=new Label(SERIES_LABEL_A_T);
+        aLabel.setTextFill(PAINT_BLACK);
+        Label rLabel=new Label(SERIES_LABEL_R_T);
         rLabel.setLayoutY(30);
-        rLabel.setTextFill(Paint.valueOf("black"));
+        rLabel.setTextFill(PAINT_BLACK);
         
         getChildren().addAll(valueAText,valueRText,rLabel,aLabel,circleSum,formula);
     }
@@ -67,20 +72,7 @@ public class Series extends AnimationBase{
     public void reset()
     {
         done();
-        timeline=new Timeline();
-        valueAText=new TextField(String.valueOf(IConstants.SERIES_A_DEF));
-        valueRText=new TextField(String.valueOf(IConstants.SERIES_R_DEF));
-        circleSum=new Circle();
-        radius=IConstants.SERIES_SUM_RADIUS_IN;
-        circleSum.setRadius(radius);
-        circleSum.setLayoutX(IConstants.SERIES_SUM_X);
-        circleSum.setLayoutY(IConstants.SERIES_SUM_Y);
-        infinity=IConstants.SERIES_INFINITY_IN;
-        valueRText.setLayoutY(30);
-        valueRText.setLayoutX(50);
-        valueAText.setLayoutX(50);
-        
-        setStaticGUI();
+        setupGui();
     }
     // stops animation so it's ready to start again
     public void done()
@@ -100,25 +92,25 @@ public class Series extends AnimationBase{
         timeline=new Timeline();
         double a=getValueA(); //gets A value
         double r=getValueR(); //gets r value
-        radius=0;
-        infinity=IConstants.SERIES_INFINITY_IN; //sets infiity var to -1, once it's higher then 0, the sum is considered tending to infinity
-        double tendValue=Math.PI;
-        double scale=IConstants.SERIES_SCALE_DEF; //scale of the circle, could make it dynamic, but keeping is like this for now
+        radius=ZERO;
+        infinity=SERIES_INFINITY_IN; //sets infiity var to -1, once it's higher then 0, the sum is considered tending to infinity
+        Double tendValue=null;
+        double scale=SERIES_SCALE_DEF; //scale of the circle, could make it dynamic, but keeping is like this for now
         
         Label radiusL=new Label(String.valueOf(radius)); //created a radius label for the big sum circle that doesnt move
-        radiusL.getStyleClass().add(IConstants.SERIES_LABEL_STYLE);
-        radiusL.setLayoutX(IConstants.SERIES_VALUE_LABEL_X);
-        radiusL.setLayoutY(IConstants.SERIES_VALUE_LABEL_Y);
+        radiusL.getStyleClass().add(SERIES_LABEL_STYLE);
+        radiusL.setLayoutX(SERIES_VALUE_LABEL_X);
+        radiusL.setLayoutY(SERIES_VALUE_LABEL_Y);
         getChildren().add(radiusL); //adds radius label
-        int iMax=0;
-        for(int i=0;i<IConstants.SERIES_MAXITERATIONS;i++)
+        int iMax=ZERO;
+        for(int i=ZERO;i<SERIES_MAXITERATIONS;i++)
         {
             
-            double value=(a*Math.pow(r, i)); //finds the value at a certain n
+            double value=getValue(a,r,i); //finds the value at a certain n
 
            // System.out.println(value);//debug
             Circle c=new Circle(); //creates a small cicle tha trepresents the value
-            c.setFill(Paint.valueOf("red"));
+            c.setFill(PAINT_RED);
             c.setOpacity(IConstants.SERIES_OPACITY_IN);
             c.setRadius(0);
             c.setLayoutY(IConstants.SERIES_VALUE_Y);//starts at 0
@@ -131,24 +123,24 @@ public class Series extends AnimationBase{
             t.setVisible(false); //will be set visible when small circle appears
             this.getChildren().addAll(c,t); //add both to screen, t being non-visible and c being of radius 0, so invisible
             
-            KeyValue valuescXI=new KeyValue(c.centerXProperty(),200);   //keyvalue for small circle X initial
-            KeyValue valuescXF=new KeyValue(c.centerXProperty(),500);   //keyvalue for small circle X final
-             KeyValue valuescOI=new KeyValue(c.opacityProperty(),0);    //keyvalue for small circle opacity initial
-            KeyValue valuescOF=new KeyValue(c.opacityProperty(),1);     //keyvalue for small circle opacity final
-            KeyValue valuescRF=new KeyValue(c.radiusProperty(),(value/scale)>20?value/scale:20); //key value for small circle radius -> becomes bigger
-            KeyValue valuetXI=new KeyValue(t.layoutXProperty(),200);    //key value for value label X initial
-            KeyValue valuetXF=new KeyValue(t.layoutXProperty(),500);    //key value for value label X final
+            KeyValue valuescXI=new KeyValue(c.centerXProperty(),SERIES_VALUECIRCLE_XI);   //keyvalue for small circle X initial
+            KeyValue valuescXF=new KeyValue(c.centerXProperty(),SERIES_VALUECIRCLE_XF);   //keyvalue for small circle X final
+             KeyValue valuescOI=new KeyValue(c.opacityProperty(),SERIES_OPACITY_IN);    //keyvalue for small circle opacity initial
+            KeyValue valuescOF=new KeyValue(c.opacityProperty(),SERIES_OPACITY_FN);     //keyvalue for small circle opacity final
+            KeyValue valuescRF=new KeyValue(c.radiusProperty(),(value/scale)>SERIES_VALUECIRCLE_RMIN?value/scale:SERIES_VALUECIRCLE_RMIN); //key value for small circle radius -> becomes bigger
+            KeyValue valuetXI=new KeyValue(t.layoutXProperty(),SERIES_VALUECIRCLE_XI);    //key value for value label X initial
+            KeyValue valuetXF=new KeyValue(t.layoutXProperty(),SERIES_VALUECIRCLE_XF);    //key value for value label X final
             
             KeyValue circleI=new KeyValue(circleSum.radiusProperty(),radius/scale); //creates a keyvalue for the circle radius initial
-            if(radius>500) // once radius is bigger then 500, it tends to a too big number
+            if(radius>SERIES_MAX_SUM_R) // once radius is bigger then 500, it tends to a too big number
             {
-                if(r>=1 || r<=-1) //iso we check if it tends to infinity
+                if(r>=SERIES_INFINITE_RANGE[1] || r<=SERIES_INFINITE_RANGE[0]) //iso we check if it tends to infinity
                 {
                    infinity=radius/scale;
                     
                 }else //if it tends to a real value
                 {
-                    tendValue=(a/(1-r)); //fiond the sum end result to display latter
+                    tendValue=(a/(1-r)); //find the sum end result to display latter
                 }
                 
             }else
@@ -156,14 +148,14 @@ public class Series extends AnimationBase{
                 radius+=value; //if its not too big continue adding
             }
             KeyValue circleF=new KeyValue(circleSum.radiusProperty(),radius/scale); //keyvalue for the circle radius final
-            KeyFrame kf0=new KeyFrame(Duration.millis(i*1000),new EventHandler<ActionEvent>(){ //first keyframe for the initial values
+            KeyFrame kf0=new KeyFrame(Duration.millis(i*TIME_MILLIS_CONVERSION),new EventHandler<ActionEvent>(){ //first keyframe for the initial values
 
                 @Override
                 public void handle(ActionEvent event) {
                     t.setVisible(true); //set value label to true when it's animation starts
                 }
             },valuescOI,valuescXI,valuetXI);
-            KeyFrame kf1=new KeyFrame(Duration.millis((i+1)*1000),new EventHandler<ActionEvent>(){ //second keyframe for the final value circle values and initial sum circle values
+            KeyFrame kf1=new KeyFrame(Duration.millis((i+1)*TIME_MILLIS_CONVERSION),new EventHandler<ActionEvent>(){ //second keyframe for the final value circle values and initial sum circle values
 
                 @Override
                 public void handle(ActionEvent event) {
@@ -171,18 +163,19 @@ public class Series extends AnimationBase{
                     getChildren().removeAll(c,t); //remove useless value circle and it's label since they already played the animation
                 }
             },valuescXF,valuescOF,valuetXF,circleI ,valuescRF);
-            final double tend=tendValue; //need final for stupid innerclasses. 
-            KeyFrame kf2=new KeyFrame(Duration.millis((i+1)*1000+500),new EventHandler<ActionEvent>(){ //third keyframe for sum circle final.
+            final Double tend=tendValue; //need final for stupid innerclasses. 
+            KeyFrame kf2=new KeyFrame(Duration.millis((i+1)*TIME_MILLIS_CONVERSION+SERIES_TIME_SUM_EX),new EventHandler<ActionEvent>(){ //third keyframe for sum circle final.
 
                 @Override
                 public void handle(ActionEvent event) {
                     DecimalFormat f = new DecimalFormat("##.00");
-                    radiusL.setText(tend!=Math.PI?f.format(tend):infinity==circleSum.getRadius()?"infinity":f.format(circleSum.getRadius()*scale)); //set new sum value. Shitty code, but no time to clean up
+                    
+                    radiusL.setText(tend!=null?f.format(tend):infinity==circleSum.getRadius()?"SERIES_TEXT_INFINITE":f.format(circleSum.getRadius()*scale)); //set new sum value. Shitty code, but no time to clean up
                 }
             },circleF);
             
             timeline.getKeyFrames().addAll(kf0,kf1,kf2); //add all keyframes to timeline. 
-            if(infinity>0 || tendValue!=Math.PI) //if too big, set maximum i for later
+            if(infinity>ZERO || tendValue!=Math.PI) //if too big, set maximum i for later
             {
                 iMax=i;
                 break;
@@ -198,13 +191,9 @@ public class Series extends AnimationBase{
         timeline.play(); //play the whole damn thing
     }
     
-    public double getValue(int n)
+    public double getValue(double a,double r,int n)
     {
-        return 0;
-    }
-    public double getInfitniteValue()
-    {
-        return 0;
+        return (a*Math.pow(r, n));
     }
     
     public double getValueA()
